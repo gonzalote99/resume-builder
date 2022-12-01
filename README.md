7e023203d8a6d780d640b325ab5f42f0db40e984
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2022-12-01 15:07:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# resume-builder
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-6a53dp)
---
+++ profile.js
@@ -0,0 +1,85 @@
+const profileData = {
+  title: 'Resume ',
+  name: 'John Doe',
+  sub_title: 'Ui Engineer',
+  logoUrl: 'https://raw.githubusercontent.com/imvpn22/resume/assets/images/dp.jpg',
+  about: {
+    intro: `front end developer, interest in <strong>javascript</strong>`,
+    contact: {
+      email: "alg@gm.com",
+      phone: "+934 6565",
+      address: "manfg, los"
+    }
+  },
+
+  links: [
+    {title: "Github", src: "https://github.com/gonzalote99"}
+  ],
+
+  education: [
+    {
+      alma: "THS , INPS",
+      duration: "2013-2015",
+      std: "b. tech",
+      score: "69%"
+    }
+  ],
+
+  skills : 
+    [
+  {
+    category: "languages",
+    topics : ["Javascript", "Typrescript"] 
+  },
+
+  {
+    category: "library/frameworks",
+    topics: ["ReactJs",  "Angular"]
+  }
+   
+],
+
+projects: [
+  {
+    title: "traffic light",
+    duration: "june-aug 2019",
+    link: "https://github.com/gonzalote99/traffic-light",
+    desc: `using html,css,js`
+
+  },
+  {
+    title: "hangma",
+    duration: "june-aug 2018",
+    link: "https://github.com/gonzalote99/hangman",
+    desc: `using html,css, js`
+  }
+
+
+
+],
+
+ experiences: [
+   {
+     organization: "pvt ltd",
+     title: "UI engineer",
+     date: "aug 2019- present",
+     detalis: [
+
+      `details here`
+     ]  
+   }
+ ],
+
+ certifications: [
+   {
+     desc: `<strong>web specialist</strong>`,
+     date: "jan - jun2019"
+   }
+ ],
+
+ events:[]
+
+
+
+
+};
